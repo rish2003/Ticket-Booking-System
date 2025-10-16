767e929530848359992376ecda521c3ae2e92408
Rish2003 <rishabhraj6421@gmail.com>
2025-10-16 22:42:34 +0530

    User-Train-Ticket classes created

---
+++ lib/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,9 @@
+package ticket.booking.entities;
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String source;
+    private String destination;
+    private Train train;
+}
